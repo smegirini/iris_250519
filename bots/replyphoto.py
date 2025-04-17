282d5990b43cbac5f4d779565ccccfa765be2252
dolidolih <gagakagaga1@gmail.com>
2025-04-17 15:02:31 +0000

    added profile image send

---
+++ bots/replyphoto.py
@@ -0,0 +1,23 @@
+from helper.Addon import is_reply
+from helper.ImageHelper import ImageHelper as ih
+from irispy2 import ChatContext
+
+def reply_photo(chat: ChatContext):
+    match chat.message.command:
+        case "!tt":
+            send_tiger(chat)
+        case "!ttt":
+            send_triple_tiger(chat)
+        case "!프사":
+            send_avatar(chat)
+
+def send_tiger(chat: ChatContext):
+    chat.reply_media("IMAGE", [open("res/aaa.jpeg", "rb")])
+
+def send_triple_tiger(chat: ChatContext):
+    chat.reply_media("IMAGE", [open("res/aaa.jpeg", "rb"), open("res/aaa.jpeg", "rb"), open("res/aaa.jpeg", "rb")])
+
+@is_reply
+def send_avatar(chat: ChatContext):
+    avatar = chat.get_source().sender.avatar.img
+    ih.send_image(chat, avatar)
