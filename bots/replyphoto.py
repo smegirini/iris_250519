1b4afde8f49a90a7b783e2bbce1bc65918e903ae
dolidolih <gagakagaga1@gmail.com>
2025-04-20 11:51:42 +0000

    added shared template info

--- bots/replyphoto.py
+++ bots/replyphoto.py
@@ -36,3 +36,49 @@ def send_avatar_kakaolink(chat: ChatContext, kl):
             "IMAGE_URL" : avatar.url
             },
     )
+
+
+{
+    "template_id": 3139,
+    "template_args": {
+        'ANDROID_EXECUTION_URL': '',
+        'COMMENT_COUNT': '', 
+        'DESCRIPTION': '', 
+        'FIRST_BUTTON_ANDROID_EXECUTION_URL': '', 
+        'FIRST_BUTTON_IOS_EXECUTION_URL': '', 
+        'FIRST_BUTTON_MOBILE_WEB_URL': '', 
+        'FIRST_BUTTON_TITLE': '', 
+        'FIRST_BUTTON_WEB_URL': '', 
+        'IMAGE_COUNT': '',
+        'IMAGE_HEIGHT': '', 
+        'IMAGE_URL': '', 
+        'IMAGE_WIDTH': '', 
+        'IOS_EXECUTION_URL': '', 
+        'ITEM1': '', 
+        'ITEM1_OP': '', 
+        'ITEM2': '', 
+        'ITEM2_OP': '', 
+        'ITEM3': '', 
+        'ITEM3_OP': '', 
+        'ITEM4': '', 
+        'ITEM4_OP': '', 
+        'ITEM5': '', 
+        'ITEM5_OP': '', 
+        'ITL_AL': '', 
+        'LIKE_COUNT': '', 
+        'MOBILE_WEB_URL': '', 
+        'PROFILE_IMAGE_URL': '', 
+        'PROFILE_TEXT1': '', 
+        'PROFILE_TEXT2': '', 
+        'SHARED_COUNT': '', 
+        'SUBSCRIBER_COUNT': '', 
+        'SUM': '', 
+        'SUM_OP': '', 
+        'TITLE': '', 
+        'TITLE_IMAGE_CATEGORY': '', 
+        'TITLE_IMAGE_TEXT': '', 
+        'TITLE_IMAGE_URL': '', 
+        'VIEW_COUNT': '', 
+        'WEB_URL': ''
+    }
+}
